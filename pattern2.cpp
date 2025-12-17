910a7923eb2633ab2fe9b6fd934ecaddba2e8e85
Manak05 <manakxkanam@gmail.com>
2025-12-17 11:56:05 +0530

    patters completed

--- pattern2.cpp
+++ pattern2.cpp
@@ -63,22 +63,104 @@ int main(){
 // }
 
 //p20
-int l=1;
-for(int i=1;i<=n;i++){
-    for(int k=1;k<=n-i;k++){
-        cout<<" ";
-    }       
-    for(int j=1;j<=i;j++){
-        cout<<l;
-    }
-    l++;
-    cout<<endl;
-}
+// int l=1;
+// for(int i=1;i<=n;i++){
+//     for(int k=1;k<=n-i;k++){
+//         cout<<" ";
+//     }       
+//     for(int j=1;j<=i;j++){
+//         cout<<l;
+//     }
+//     l++;
+//     cout<<endl;
+// }
 
 //p21
-int l=1;
-for(int i=)
+// int l=1;
+// for(int i=1;i<=n;i++){
+//         for(int k=1;k<=n-i;k++){
+//             cout<<" ";
+//         }
+//     for(int j=1;j<=i;j++){
+//         cout<<l;
+//     }
+//     l++;
+//     cout<<endl;
+// }
 
+//p22
+// 1234
+//  234
+//   34
+//    4
+// int l=1;
+// for(int i=n;i>=1;i--){
+//     for(int k=1;k<=n-i;k++){
+//         cout<<" ";
+//     }     
+//     for(int j=1;j<=i;j++){
+//         cout<<j+l-1;
+//     }  
+//     l++;
+//     cout<<endl;
+// }
+
+//p23
+//     1
+//    23
+//   456
+// 78910   
+// int g=1;
+// for(int i=1;i<=n;i++){
+//     for(int k=0;k<=n-i;k++){
+//         cout<<" ";
+//     }
+//     for(int j=1;j<=i;j++){
+//         cout<<g;
+//         g++;
+//     }
+//     cout<<endl;
+// }
+
+//p24
+//    1   
+//   121  
+//  12321
+// 1234321
+// for(int i=1;i<=n;i++){
+//     for(int k=1;k<=n-i;k++){
+//         cout<<" ";
+//     }
+//     for(int m=1;m<=i;m++){
+//         cout<<m;
+//     }
+//     int j=i-1;
+//     while(j){
+//         cout<<j;
+//         j--;
+//     }
+//     cout<<endl;
+// }
+
+      for (int i = 1; i <= n; i++) {
+
+        // left numbers
+        for (int j = 1; j <= n - i + 1; j++) {
+            cout << j;
+        }
+
+        // stars
+        for (int s = 1; s <= 2 * (i - 1); s++) {
+            cout << "*";
+        }
+
+        // right numbers
+        for (int j = n - i + 1; j >= 1; j--) {
+            cout << j;
+        }
+
+        cout << endl;
+    }   
 
 
 
