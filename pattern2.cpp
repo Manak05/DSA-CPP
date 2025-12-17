a547ed05617f64ae3fbbd27227c2bd83e4dfc7dd
Manak05 <manakxkanam@gmil.com>
2025-12-17 10:23:31 +0530

    pattern 20

--- pattern2.cpp
+++ pattern2.cpp
@@ -7,13 +7,87 @@ int main(){
     cin>>n;
 
     //p15
-    char D='D';
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=i;j++){
-                    cout<<char(D-i+j);
-                }
-                cout<<endl;
-            }
+    // char D='D';
+    //         for(int i=1;i<=n;i++){
+    //             for(int j=1;j<=i;j++){
+    //                 cout<<char(D-i+j);
+    //             }
+    //             cout<<endl;
+    //         }
+
+//p16
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int k=1;k<=n-i;k++){
+    //          cout<<" ";
+    //     }
+    //         for(int j=1;j<=i;j++){
+    //          cout<<"*";
+    //         }
+        
+    //     cout<<endl;
+    // }
+
+    //p17
+    // for(int i=n;i>=1;i--){
+    //         for(int j=1;j<=i;j++){
+    //          cout<<"*";
+    //         }
+        
+    //     cout<<endl;
+    // }   
+
+//p18
+    // for(int i=n;i>=1;i--){
+    //     for(int k=0;k<=n-i;k++){
+    //          cout<<" ";
+    //     }
+    //         for(int j=1;j<=i;j++){
+    //          cout<<"*";
+    //         }
+        
+    //     cout<<endl;
+    // }
+ 
+//p19
+// int l=1;
+// for(int i=n;i>=1;i--){
+//     for(int k=1;k<=n-i;k++){
+//         cout<<" ";
+//     }
+//     for(int j=1;j<=i;j++){
+//         cout<<l;
+//     }
+//     l++;
+//     cout<<endl;
+// }
+
+//p20
+int l=1;
+for(int i=1;i<=n;i++){
+    for(int k=1;k<=n-i;k++){
+        cout<<" ";
+    }       
+    for(int j=1;j<=i;j++){
+        cout<<l;
+    }
+    l++;
+    cout<<endl;
+}
+
+//p21
+int l=1;
+for(int i=)
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
